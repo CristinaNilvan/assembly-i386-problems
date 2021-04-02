57eaae404c6cb40aa00f16eb13be9b2af3d26a61
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-04-02 17:42:28 +0300

    Create number_power_of_two.asm

---
+++ number_power_of_two.asm
@@ -0,0 +1,34 @@
+.386
+.model flat, stdcall
+
+includelib msvcrt.lib
+extern exit: proc
+
+public start
+
+.data
+
+number dd 7
+
+.code
+start:
+
+	;Checks if a number is a power of two.
+
+	mov ebx, number		
+	mov ecx, number
+	
+	sub ebx, 1		
+	
+	and ecx, ebx	
+
+	cmp ecx, 0		
+	jne not_equal			
+	
+	mov eax, 0
+	
+	not_equal :
+	
+	push 0
+	call exit
+end start
