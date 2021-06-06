a828b7a36e9c1e2fbb1a6b6ce27b8898edc877ca
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-06-06 22:22:48 +0300

    Create vector_of_pairs.asm

---
+++ vector_of_pairs.asm
@@ -0,0 +1,47 @@
+.386
+.model flat, stdcall
+
+includelib msvcrt.lib
+extern exit: proc
+
+public start
+
+.data
+
+pair_struct struct 
+	first_number dd 0
+	second_number dd 0
+pair_struct ends
+
+number_of_pairs dd 3
+pairs pair_struct {1, 2},
+	{2, 4},
+	{3, 8}
+	
+result dd 30 dup (0)
+
+.code
+start:
+	
+	mov ecx, 0
+	mov esi, 0
+	mov edi, 0
+	
+	for_loop :
+		mov edx, 0
+		
+		add edx, pairs[esi].first_number
+		add edx, pairs[esi].second_number
+		add esi, sizeof pair_struct
+		
+		mov result[edi], edx
+		add edi, 4
+		
+		add ecx, 1
+		
+		cmp ecx, number_of_pairs
+		jne for_loop
+	
+	push 0
+	call exit
+end start
